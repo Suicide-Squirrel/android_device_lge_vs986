77589cc8651a151ec95783bb66b7a7adb2524355
steadfasterX <steadfastX@boun.cr>
2018-04-06 09:44:21 +0200

    fix: vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,7 @@
-add_lunch_combo cm_h811-userdebug
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export USE_NINJA=true
+
+add_lunch_combo aoscp_vs986_usu-userdebug
+add_lunch_combo aoscp_vs986_usu-eng
+
